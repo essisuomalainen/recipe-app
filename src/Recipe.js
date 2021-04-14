407c9e33f56442010753eb3b98cbe70a04fa0dec
Essi Suomalainen <essi.suomalainen@hyperisland.se>
2021-04-14 20:34:12 +0200

    updated stylin

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import style from './recipe.module.css';
 
-const Recipe = ({title, calories, image, ingredients}) => {
-    return (
-        <div className={style.recipe}>
-            <h1>{title}</h1>
-            <ol>
-                {ingredients.map(ingredient => (
-                    <li>{ingredient.text}</li>
-                ))}
-            </ol>
-            <p>{calories}</p>
-            <img className={style.image} src={image} alt=""></img>
-        </div>
-    )
-}
+const Recipe = ({ title, calories, image, ingredients }) => {
+  return (
+    <div className={style.recipe}>
+      <h1>{title}</h1>
 
-export default Recipe
+      <h2>Ingredients:</h2>
+      <ol>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ol>
+
+      <p>Calories: {calories}</p>
+      <img className={style.image} src={image} alt=''></img>
+    </div>
+  );
+};
+
+export default Recipe;
